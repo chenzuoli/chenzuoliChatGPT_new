123940753428882c7c3b63c20bf0932e4a9a5c8e
chenzuoli <chenzuoli709@163.com>
2023-04-03 21:41:33 +0800

    allow other domains to access the api.

--- api/index.js
+++ api/index.js
@@ -24,6 +24,10 @@ app.use(require("morgan")("dev"));
 // Primary Open AI Route
 app.post("/", async (req, res) => {
   // console.log("OpenAI API Key: " + process.env.OPENAI_API_KEY);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
   const { message } = req.body;
   console.log(req + ":" + `${message}`)
